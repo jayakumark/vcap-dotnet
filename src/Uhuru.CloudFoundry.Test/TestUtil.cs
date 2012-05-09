a36e3d2197cc09f1e0369a378cd76ff24b4f8800
Vlad Iovanov <vlad.iovanov@uhurusoftware.com>
2012-05-09 15:31:09 +0300

    Update AUTHORS test refactor to use addaptor. Initial commit fix for tests

--- src/Uhuru.CloudFoundry.Test/TestUtil.cs
+++ src/Uhuru.CloudFoundry.Test/TestUtil.cs
@@ -7,11 +7,122 @@ using System.Net;
 using System.IO;
 using System.Xml;
 using System.Threading;
+using Uhuru.CloudFoundry.Adaptor.Objects;
+using Uhuru.CloudFoundry.Adaptor.Objects.Packaging;
+using Uhuru.CloudFoundry.Adaptor;
+using System.Configuration;
+using System.Security;
 
 namespace Uhuru.CloudFoundry.Test
 {
     class TestUtil
     {
+        static List<Guid> currentJobs = new List<Guid>();
+        static readonly object locker = new object();
+        public static void DeleteUser(string username, List<string> directoriesCreated)
+        {
+            string target = ConfigurationManager.AppSettings["target"];
+            CloudCredentialsEncryption encryptor = new CloudCredentialsEncryption();
+            SecureString encryptedPassword = encryptor.Decrypt(ConfigurationManager.AppSettings["adminPassword"].ToString());
+            CloudManager cloudManager = CloudManager.Instance();
+            CloudTarget cloudTarget = new CloudTarget(ConfigurationManager.AppSettings["adminUsername"].ToString(), encryptedPassword, new Uri(target));
+            CloudConnection cloudConnection = cloudManager.GetConnection(cloudTarget);
+
+            User tempUser = cloudConnection.Users.First(usr => usr.Email == username);
+            tempUser.Delete();
+            foreach (string str in directoriesCreated)
+            {
+                Directory.Delete(str, true);
+            }
+        }
+
+        public static CloudConnection CreateAndImplersonateUser(string username,string password)
+        {
+            string target = ConfigurationManager.AppSettings["target"];
+            CloudCredentialsEncryption encryptor = new CloudCredentialsEncryption();
+            ///SecureString ss = new SecureString();
+             //ConfigurationManager.AppSettings["adminPassword"].ToString()
+            SecureString encryptedPassword = CloudCredentialsEncryption.GetSecureString(ConfigurationManager.AppSettings["adminPassword"].ToString());
+            CloudManager cloudManager = CloudManager.Instance();
+            CloudTarget cloudTarget = new CloudTarget(ConfigurationManager.AppSettings["adminUsername"].ToString(), encryptedPassword, new Uri(@"http://"+target));
+            CloudConnection cloudConnection = cloudManager.GetConnection(cloudTarget);
+
+            cloudConnection.CreateUser(username, password);
+
+            SecureString newPassword = CloudCredentialsEncryption.GetSecureString(password);
+            cloudTarget = new CloudTarget(username, newPassword, new Uri(@"http://"+target));
+
+            cloudConnection = cloudManager.GetConnection(cloudTarget);
+
+            return cloudConnection;
+        }
+
+
+        public static void PushApp(string appName, string sourceDir, string url, List<string> directoriesCreated, CloudConnection cloudConnection)
+        {
+            PushApp(appName, sourceDir, url, directoriesCreated, cloudConnection, null, null, null);
+        }
+
+        public static void PushApp(string appName, string sourceDir, string url, List<string> directoriesCreated, CloudConnection cloudConnection, string vendor ,string serviceName, string path)
+        {
+            if (path == null)
+                path = TestUtil.CopyFolderToTemp(sourceDir);
+            directoriesCreated.Add(path);
+
+            if (serviceName == null)
+                serviceName = appName + "svc";
+
+            CloudApplication cloudApp = new CloudApplication()
+            {
+                Name = appName,
+                Urls = new string[1] { url },
+                DeploymentPath = path,
+                Deployable = true,
+                Framework = "dotNet",
+                InstanceCount = 1,
+                Memory = 128,
+                Runtime = "iis",
+            };
+            PushTracker pushTracker = new PushTracker();
+            Guid tracker = Guid.NewGuid();
+            pushTracker.TrackId = tracker;
+            currentJobs.Add(tracker);
+            cloudConnection.PushJob.Start(pushTracker, cloudApp);
+            if (vendor != null)
+            {
+                cloudConnection.CreateProvisionedService(cloudConnection.SystemServices.FirstOrDefault(ss => ss.Vendor == vendor), serviceName, true);
+                Thread.Sleep(1000);
+            }
+
+            cloudConnection.PushJob.JobCompleted += new EventHandler<global::System.ComponentModel.AsyncCompletedEventArgs>(PushJob_JobCompleted);
+
+            while (currentJobs.Contains(tracker))
+            {
+                Thread.Sleep(1000);
+            }
+
+            App currentApp = cloudConnection.Apps.FirstOrDefault(app => app.Name == cloudApp.Name);
+
+            ProvisionedService provisionedService = cloudConnection.ProvisionedServices.FirstOrDefault(ps => ps.Name == serviceName);
+            if (vendor != null)
+            {
+                currentApp.BindService(provisionedService);
+                Thread.Sleep(1000);
+            }
+            currentApp.Start();
+
+
+        }
+
+        static void PushJob_JobCompleted(object sender, global::System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            lock (locker)
+            {
+                currentJobs.Remove((Guid)((PushTracker)e.UserState).TrackId);
+            }
+            //throw new NotImplementedException();
+        }
+
         public static string CopyFolderToTemp(string folder)
         {
             string tempFolder = Path.GetTempPath();
